ef710131636392ff24704fdab6a1b73ae67ba8e3
bmchae <bmchae@gmail.com>
2012-04-19 14:27:35 +0900

    04/19 -

---
+++ src/main/java/com/nexr/platform/analysis/App.java
@@ -0,0 +1,13 @@
+package com.nexr.platform.analysis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
